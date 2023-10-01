d30daaa5e9cb932eaba217d1efaf0253f44b0b92
MunJunHo <mjh1995713@gmail.com>
2023-10-01 17:40:24 +0900

    맵 만들기

--- Algorithm/02_Maze/Maze.cpp
+++ Algorithm/02_Maze/Maze.cpp
@@ -3,9 +3,18 @@
 #include"pch.h"
 #include <iostream>
 #include"ConsoleHelper.h"
+#include"Board.h"
+#include"Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
+	::srand(static_cast<unsigned>(time(nullptr)));
+	board.Init(25, &player);
+	player.Init(&board);
+
 	uint64 lastTick = 0;
 
 	while (true)
@@ -19,23 +28,12 @@ int main()
 		// 입력
 
 		// 로직
+		player.Update(deltaTick);
 
 		// 렌더링
 
-		ConsoleHelper::SetCursorPosition(0, 0);
-		ConsoleHelper::ShowConsoleCursor(false);// 깜박임 끄기
-		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+		board.Render();
 
-		const char* TILE = "■";
-
-		for (int32 y = 0; y < 25; y++)
-		{
-			for (int32 x = 0; x < 25; x++)
-			{
-				cout << TILE;
-			}
-			cout << endl;
-		}
 
 	}
 
