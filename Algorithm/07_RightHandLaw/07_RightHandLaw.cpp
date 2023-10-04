81143546853b4eff4e41f8a319a972d6e5c4dc58
MunJunHo <mjh1995713@gmail.com>
2023-10-04 15:27:51 +0900

    스택을 이용해서 오른손 법칙 개선

---
+++ Algorithm/07_RightHandLaw/07_RightHandLaw.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+#include<list>
+#include<vector>
+#include<queue>
+using namespace std;
+
+// 동적 배열
+// push_back O(1)
+// 중간 삽입/삭제 O(N)
+// 임의 접근 O(1)
+// [][][][][][]
+
+// 연결 리스트
+// 삽입/삭제 O(1)
+// 임의 접근 O(N)
+
+
+// 스택 : 삽입/삭제 O(1)
+// 큐 : 삽입/삭제 O(1)
+
+
+int main()
+{
+
+
+	return 0;
+}
