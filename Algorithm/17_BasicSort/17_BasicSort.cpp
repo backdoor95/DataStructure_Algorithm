57fcf33ec08f798f9ac156104ea0d966c2d1a6b4
MunJunHo <mjh1995713@gmail.com>
2023-10-11 19:43:26 +0900

    Heap sort, Merge sort

--- Algorithm/17_BasicSort/17_BasicSort.cpp
+++ Algorithm/17_BasicSort/17_BasicSort.cpp
@@ -13,11 +13,12 @@ using namespace std;
 // 왜 버블인가?
 // 하나 단위로 거품단위로 동작하는 느낌이기 때문
 // 직관적이고 구현하는게 쉽다. 단, 효율성은 최악이다.
+// [stable sort] [in-place]
 void BubbleSort(vector<int>& v)
 {
 	const int n = (int)v.size();
 
-	// (N-1)+(N-2)+....+2+1 = N*(N-1)/2 ==O(N^2) -> 현실적으로 실전에서는 못씀.
+	// 시간복잡도 : (N-1)+(N-2)+....+2+1 = N*(N-1)/2 ==O(N^2) -> 현실적으로 실전에서는 못씀.
 
 	for (int i = 0; i < n-1; i++)
 	{
@@ -34,9 +35,16 @@ void BubbleSort(vector<int>& v)
 }
 
 // 2) 선택 정렬(Selection Sort)
+// 
+// 선택정렬(selection sort) (O(n^2))
+// (1) 주어진 배열에서 최솟값을 찾는다.
+// (2) 그 최솟값을 맨 앞에 위치한 것과 바꾼다.(swap) --> 이것때문에 not stable 인듯하다.
+// (3) 맨 앞의 것을 제외하고, 나머지 것들을 대상으로 다시 위(1)~(2) 과정을 반복한다.
+// 
 // 스캔을 하면서 가장 작은놈을 왼쪽에 배치
 // 그 다음에 작은놈을 2번째 왼쪽에 배치
 // 버블정렬은 2개를 비교함..
+// [not stable] [in-place] 
 void SelectionSort(vector<int>& v)
 {
 	const int n = (int)v.size();
@@ -44,7 +52,7 @@ void SelectionSort(vector<int>& v)
 	for (int i = 0; i < n-1; i++)
 	{
 		int bestIdx = i;
-		for (int j = i+1; j < n; j++)
+		for (int j = i + 1; j < n; j++)
 		{
 			if (v[j] < v[bestIdx])
 				bestIdx = j;
@@ -61,6 +69,7 @@ void SelectionSort(vector<int>& v)
 // 코딩면접에서 잘나옴.
 // 삽입정렬에 사용된 테크닉이 코딩문제에서도 잘나옴.
 // 느낌이 선택정렬과 비슷함.
+// 정렬된 데이터를 만들어 나가는 느낌으로 동작을 하게 됩니다
 // 만약 운이 좋게 대부분의 데이터들이 정렬이 되있는 상태라면 더 효율적으로 동작할수 있는 확률이 있다.
 // 일반적인 상황에서는 삽입 정렬이 딱히 효율적이지는 않다.
 void InsertionSort(vector<int>& v)
@@ -89,8 +98,8 @@ int main()
 {
 	vector<int> v{ 1,5,3,4,2 };
 
-	BubbleSort(v);
-	SelectionSort(v);
+	//BubbleSort(v);
+	//SelectionSort(v);
 	InsertionSort(v);
 
 	return 0;
---
+++ Algorithm/18_HeapSort&MergeSort/18_HeapSort&MergeSort.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+// 오늘의 주제 : 힙 정렬| 병합 정렬
+
+// 힙 정렬
+void HeapSort(vector<int>& v)
+{
+	//힙 구조는 최소&최대에 해당하는 데이터를 뽑을때 굉장히 최적화가 되어있다.
+	// priority_queue는 힙 구조으로 되어있다. 
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	// O(N*logN)
+	for (int num : v)
+	{
+		pq.push(num);
+	}
+
+	v.clear();
+
+	// O(N*logN)
+	while (pq.empty() == false)
+	{
+		v.push_back(pq.top());
+		pq.pop();
+	}
+
+}
+
+// 병합 정렬
+// 분할 정복 (Divide and Conquer)
+// - 분할 (Divide)		문제를 더 단순하게 분할한다.
+// - 정복 (Conquer)		분할된 문제를 해결
+// - 결합 (Combine)		결과를 취합하여 마무리
+
+// 문제를 분할해서 각각 다른 애들이 실행을 하게끔 만들어줄수 있으니까
+// 나중에 멀티스레드 환경에서도 굉장히 유용함.
+
+// [3][K][7][2][J][4][8][9]					8개 * 1
+// [3][K][7][2]  [J][4][8][9]				4개 * 2
+// [3][K]  [7][2]  [J][4]  [8][9]			2개 * 4
+// [3]  [K]  [7]  [2]  [J]  [4]  [8]  [9]	1개 * 8
+// [3][K]  [2][7]  [4][J]  [8][9]			2개 * 4
+// [2][3][7][K]  [4][8][9][J]  				4개 * 2
+// [2][3][4][7][8][9][J][K]					8개 * 1
+
+// MergeResult의 시간복잡도 : O(N) 
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+
+	vector<int> temp;
+
+	while (leftIdx <= mid && rightIdx <= right)
+	{
+		if (v[leftIdx] <= v[rightIdx])
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+		else
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+
+	// 왼쪽이 먼저 끝났으면, 오른쪽 나머지 데이터 복사
+	if (leftIdx > mid)
+	{
+		while (rightIdx <= right)
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	else// 오른쪽이 먼저 끝났으면, 왼쪽 나머지 데이터 복사
+	{
+		while (leftIdx <= mid)
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+	}
+
+	for (int i = 0; i < temp.size(); i++)
+	{
+		v[left + i] = temp[i];
+	}
+
+}
+
+// MergeSort의 시간복잡도 : O(logN)
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right)
+		return;
+
+	int mid = (left + right)/ 2;
+	MergeSort(v, left, mid);// 4
+	MergeSort(v, mid + 1, right);// 4
+
+	MergeResult(v, left, mid, right);
+}
+
+int main()
+{
+	vector<int> v;
+
+	srand(time(0));
+
+	for (int i = 0; i < 50; i++)
+	{
+		int randValue = rand() % 100;
+		v.push_back(randValue);
+	}
+
+	//HeapSort(v);
+
+	MergeSort(v, 0, v.size() - 1);
+}
+
