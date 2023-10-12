a495abd9c24f98dbcf04b44308a7617ac3401923
MunJunHo <mjh1995713@gmail.com>
2023-10-12 19:31:56 +0900

    Prim

--- Algorithm/02_Maze/Board.h
+++ Algorithm/02_Maze/Board.h
@@ -16,17 +16,6 @@ enum class TileType
 	WALL,
 };
 
-struct CostEdge
-{
-	int cost;
-	Pos u;
-	Pos v;
-
-	bool operator<(CostEdge& other)
-	{
-		return cost < other.cost;
-	}
-};
 
 class Board
 {
@@ -37,7 +26,9 @@ public:
 	void Init(int32 size, Player* player);
 	void Render();
 
-	void GenerateMap();
+	void GenerateMap_Kruskal();
+	void GenerateMap_Prim();
+
 	TileType GetTileType(Pos pos);
 	ConsoleColor GetTileColor(Pos pos);
 
---
+++ Algorithm/23_Prim/23_Prim.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+
+/*
+PRIM MST 알고리즘
+
+하나의 시작점으로 구성된 트리에 간선을 하나씩 수집하며 진행
+
+
+- 다익스트라 알고리즘이랑 종이 한장 차이
+-- 다익스트라는 시작점을 기준 최단 cost
+-- 프림은 트리(정점 집합)을 기준 최단 cost
+
+
+
+
+*/
+int main()
+{
+    std::cout << "Hello World!\n";
+    // 
+}
+
