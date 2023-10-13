2a90133a24da4c0bff9fed3f2a206a24caf2a6aa
MunJunHo <mjh1995713@gmail.com>
2023-10-13 21:15:43 +0900

    enchant

---
+++ Algorithm/28_Enchant/28_Enchant.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<thread>
+#include<windows.h>
+
+using namespace std;
+
+// 오늘의 주제 : 동적계획법 dp
+// enchant
+
+// 집행검 0강
+
+// +N강 집행검 뜨는 경우의 수는?
+// 무기 강화 주문서 -> +1/ +2 / +3 중 하나
+
+
+int N;
+int cache[100];
+
+// +num 에서 +N 까지 가는 경우의 수
+int Enchant(int num)
+{
+	// 기저 사례
+	if (num > N)
+		return 0;
+
+	if (num == N)
+		return 1;
+
+	// 캐시
+	int& ret = cache[num];
+	if (ret != -1)
+		return ret;
+
+
+	// 적용
+	Enchant(num + 1);
+
+}
+
+
+int main()
+{
+	N = 9;
+
+	memset(cache, -1, sizeof(cache));
+
+}
+
